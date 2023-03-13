4647dc48fc342e2d1b90a7ab52e3cd1de7783069
Crostobol <cristobal.oyarzun@cloud.uautonoma.cl>
2023-03-13 15:27:25 -0300

    rutas y endpoints para el CRUD de conductor

---
+++ backend/src/controllers/index.controller.js
@@ -0,0 +1,71 @@
+const {Pool}= require('pg')
+
+const pool = new Pool({
+    host: 'localhost',
+    user:'postgres',
+    password:'tobal2.3',
+    database:'TTDB',
+    port: '5432'
+
+})
+
+const getConductors = async (req, res) =>{
+   const response =  await  pool.query('SELECT * FROM conductor');
+   res.status(200).json(response.rows)
+};
+
+const postConductors = async (req, res) =>{
+    
+    const {conductor_id,movil_id} = req.body
+    const response =  await  pool.query('INSERT INTO conductor (conductor_id, movil_id) VALUES ($1,$2)',[conductor_id,movil_id]);
+    res.json({
+        message: `Conductor ${conductor_id} creado correctamente`,
+        body: {
+            user:{conductor_id, movil_id}
+        }
+    })
+ };
+
+ const getConductorById = async (req,res) => {
+    const response =  await  pool.query('SELECT * FROM conductor where conductor_id = $1',[req.params.conductor_id]);
+    res.status(200).json(response.rows)
+ };
+
+ //endpoint delete conductor, solo se borra este, ningun otro dato asociado al mismo.
+ const deleteConductorById = async (req, res) => {
+    console.log(req.params)
+    const conductor_id = req.params.conductor_id
+    const movil_id = req.params.movil_id
+    const response = await pool.query('DELETE FROM conductor where conductor_id = $1 ', [conductor_id]);
+    res.json({
+        message: `Conductor ${conductor_id} eliminado correctamente`,
+        body: {
+            user:{conductor_id, movil_id}
+        }
+    })
+ };
+
+ const updateConductor = async (req,res) => {
+    const conductor_Id = req.params.conductor_id
+    const { movil_id,conductor_id} = req.body
+    const response = await pool.query('UPDATE conductor SET movil_id = $1, conductor_id=$2 WHERE conductor_id = $3', [movil_id,conductor_id, conductor_Id]);
+    
+    res.status(200).json({
+        message: `Conductor ${conductor_Id} actualizado correctamente`,
+        body: {
+            user:{conductor_id, movil_id}
+        }
+    })
+
+ }
+
+
+
+module.exports = {
+    getConductors,
+    postConductors,
+    getConductorById,
+    deleteConductorById,
+    updateConductor,
+    pool
+}
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,17 @@
 const { Router } = require('express')
 const router = Router();
+const {getConductors, postConductors,getConductorById,deleteConductorById,updateConductor} = require('../controllers/index.controller')
+const {getInventories} = require('../controllers/movil.controller')
 
-router.get('/conductors',(req, res) =>{
-    res.send('conductors')
-})
+//Conductores
+router.get('/conductors', getConductors)
+router.get('/conductors/:conductor_id', getConductorById)
+router.post('/conductors', postConductors)
+router.delete('/conductors/:conductor_id',deleteConductorById)
+router.put('/conductors/:conductor_id',updateConductor)
 
-
+//Inventario
+router.get('/movil', getInventories)
 
 module.exports = router
 
